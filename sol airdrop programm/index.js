512e8f654c812b7eb140abcd927f5589d5bf2d00
void-ness <78547746+void-ness@users.noreply.github.com>
2022-01-05 17:38:09 +0530

    init commit

---
+++ sol airdrop programm/index.js
@@ -0,0 +1,55 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    Account,
+   } = require("@solana/web3.js");
+
+   const newPair = new Keypair();
+   const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+   const secretKey = newPair._keypair.secretKey;
+
+//    console.log("Your public key is ",publicKey);
+//    console.log("Your secret key is ",secretKey);
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`=> For wallet address ${publicKey}`);
+        // console.log(walletBalance);
+        console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = Keypair.fromSecretKey(secretKey);
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+        // getWalletBalance();
+        // console.log("done")
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const driverFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    // await airDropSol();
+    await getWalletBalance();
+}
+driverFunction();
